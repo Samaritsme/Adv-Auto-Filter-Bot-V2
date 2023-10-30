11c890aa2d759ef573fac97bad51cc3c195692e1
Samaritsme <136054553+Samaritsme@users.noreply.github.com>
2023-10-31 01:10:30 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/smd_owner"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/smd_owner'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/smd_owner')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/SAM_DUB_LEZHa')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
